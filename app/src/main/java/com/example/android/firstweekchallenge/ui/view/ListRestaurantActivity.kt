23f4846562385941c530a8a1509653fe27b4a087
khangdong-github <khangdongduy97@gmail.com>
2021-04-22 15:29:52 +0700

    make menu options

--- app/src/main/java/com/example/android/firstweekchallenge/ui/view/ListRestaurantActivity.kt
+++ app/src/main/java/com/example/android/firstweekchallenge/ui/view/ListRestaurantActivity.kt
@@ -3,9 +3,13 @@ package com.example.android.firstweekchallenge.ui.view
 
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.firstweekchallenge.R
 import com.example.android.firstweekchallenge.data.RestaurantAdapter
@@ -24,9 +28,29 @@ class ListRestaurantActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(RestaurantViewModel::class.java)
         adapter = RestaurantAdapter()
         binding.rcList.layoutManager = LinearLayoutManager(this)
+        //binding.rcList.layoutManager = GridLayoutManager(this,2)
         binding.rcList.adapter = adapter
         adapter.submitList(getData())
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.nav_list_opt->{
+                Toast.makeText(this,"ListView Selected",Toast.LENGTH_SHORT).show()
+                binding.rcList.layoutManager = LinearLayoutManager(this)}
+            R.id.nav_grid_opt->{
+                Toast.makeText(this,"GridView Selected",Toast.LENGTH_SHORT).show()
+                binding.rcList.layoutManager = GridLayoutManager(this,2)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
 
 
